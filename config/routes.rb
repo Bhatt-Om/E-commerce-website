e4e8142cb0bf6b178441c9a7919bf8cfd7ec3225
Bhatt-Om <bhattom1111@gmail.com>
2023-04-21 13:33:09 +0530

    Generate-product-action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/edit'
+  get 'products/create'
+  get 'products/update'
+  get 'products/destroy'
   root 'pages#home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
